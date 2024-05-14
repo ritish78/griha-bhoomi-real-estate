d173d012e55cd610ed9ffbae14a589362c6c437d
ritish78 <ritishtimalsina78@gmail.com>
2024-05-14 11:49:50 +0545

    Added more ui components of the nav and also made changes when the screen size is small

--- client/src/config/siteConfig.ts
+++ client/src/config/siteConfig.ts
@@ -6,9 +6,24 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "GrihaBhoomi",
-  description: "An opensource real-estate website built using Nextjs 14",
+  description: "An opensource real-estate website built using Nextjs 14.",
   links,
   mainNav: [
+    {
+      title: "Home",
+      items: [
+        {
+          title: "Rent",
+          description: "View or add property for rent!",
+          items: []
+        },
+        {
+          title: "Buy",
+          description: "View property that you want to buy or, add property for sale!",
+          items: []
+        }
+      ]
+    },
     {
       title: "Rent",
       items: [
