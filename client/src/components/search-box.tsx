8f32de382e91a3cd83681d271680b820a2620995
ritish78 <ritishtimalsina78@gmail.com>
2024-12-10 20:49:28 +0545

    Search Box redirects to the search page

--- client/src/components/search-box.tsx
+++ client/src/components/search-box.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { Icons } from "./icons";
-
+import { ListOfPropertiesResponse, ListOfPropertiesSuccess, Property } from "@/types/property";
 import { cn, isMacOS } from "@/lib/utlis";
 import { Button } from "@/components/ui/button";
 import {
@@ -17,6 +17,11 @@ import {
 } from "@/components/ui/command";
 import { useDebounce } from "@/hooks/use-debounce";
 import { Kbd } from "@/components/kbd";
+import { getFilteredListOfProperties } from "@/actions/property";
+
+// type PropertyGroup = NonNullable<
+//   Awaited<ReturnType<typeof getFilteredListOfProperties>>["property"]
+// >[number];
 
 export function SearchBox() {
   //TODO: Implement Search functionality
@@ -24,31 +29,41 @@ export function SearchBox() {
   const [open, setOpen] = useState(false);
   const [query, setQuery] = useState("");
   const debouncedQuery = useDebounce(query, 500); //Current delay is 500ms
+  const [propertiesData, setPropertiesData] = useState<ListOfPropertiesSuccess | null>(null);
   const [loading, setLoading] = useState(false);
 
   //TODO: Implement Search product in useEffect using debounced value
-  //   useEffect(() => {
-  //     if (debouncedQuery.length <= 0) {
-  //       //Then we set the data to null
-  //       //The data variable needs to be one state variable
-  //       setData(null);
-  //       return;
-  //     }
+  useEffect(() => {
+    if (debouncedQuery.length <= 0) {
+      //Then we set the data to null
+      //The data variable needs to be one state variable
+      setPropertiesData(null);
+      return;
+    }
 
-  //     async function fetchData() {
-  //       setLoading(true);
+    async function fetchData() {
+      setLoading(true);
 
-  //       //Then the logic to fetch data from the server
+      //Then the logic to fetch data from the server
+      const listOfFilteredProperty: ListOfPropertiesResponse = await getFilteredListOfProperties(
+        `keyword=${debouncedQuery}`,
+        6
+      );
 
-  //       //Then we set the data variable to the value that we received from the server
-  //       setData();
-  //       setLoading(false);
-  //     }
+      if ("error" in listOfFilteredProperty) {
+        setLoading(false);
+        return;
+      }
 
-  //     void fetchData(); //why `void fetchData()`?
-  //     //In the console, it prints:
-  //     //Warning: An effect function must not return anything besides a function, which is used for clean-up. You returned: Promise
-  //   }, [debouncedQuery])
+      //Then we set the data variable to the value that we received from the server
+      setPropertiesData(listOfFilteredProperty);
+      setLoading(false);
+    }
+
+    void fetchData(); //why `void fetchData()`?
+    //In the console, it prints:
+    //Warning: An effect function must not return anything besides a function, which is used for clean-up. You returned: Promise
+  }, [debouncedQuery]);
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
@@ -66,12 +81,25 @@ export function SearchBox() {
     callback();
   }, []);
 
+  //Remove if search box will be shown instead of redirecting the user
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        router.push("/property/search");
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
     <>
       <Button
         variant="outline"
         className="relative size-9 p-0 xl:h-10 xl:w-60 xl:justify-start xl:px-3 xl:py-2"
-        onClick={() => setOpen(true)}
+        // onClick={() => setOpen(true)}
+        onClick={() => router.push("/property/search")}
       >
         <MagnifyingGlassIcon className="size-4 xl:mr-2" aria-hidden="true" />
         <span className="hidden xl:inline-flex">Search property...</span>
@@ -83,7 +111,7 @@ export function SearchBox() {
           {isMacOS() ? "⌘" : "Ctrl"} K
         </Kbd>
       </Button>
-      <CommandDialog
+      {/* <CommandDialog
         open={open}
         onOpenChange={(open) => {
           setOpen(open);
@@ -98,12 +126,29 @@ export function SearchBox() {
             No property found.
           </CommandEmpty>
           {loading ? (
-            <div className="space-y-1 overflow-hidden px-1 py-2">Loading</div>
+            <div className="space-y-1 overflow-hidden px-1 py-2">Loading...</div>
           ) : (
-            "Your searched term results:"
+            <CommandGroup>
+              {propertiesData?.properties?.map((item: Property) => (
+                <CommandItem
+                  key={item.id}
+                  className="h-9 mb-1.5"
+                  value={item.title}
+                  onSelect={() => onSelect(() => router.push(`/property/${item.slug}`))}
+                >
+                  {item.property_type == "House" ? (
+                    <Icons.house className="mr-3 size-3 text-muted-foreground" aria-hidden="true" />
+                  ) : (
+                    <Icons.land className="mr-3 size-3 text-muted-foreground" aria-hidden="true" />
+                  )}
+
+                  <span className="truncate"> {item.title}</span>
+                </CommandItem>
+              ))}
+            </CommandGroup>
           )}
         </CommandList>
-      </CommandDialog>
+      </CommandDialog> */}
     </>
   );
 }
