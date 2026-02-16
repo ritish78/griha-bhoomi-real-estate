aba71e5846c89e3a60f7a1a8dc0ea613e1414f0b
ritish78 <ritishtimalsina78@gmail.com>
2026-02-16 19:22:24 +0545

    Added tests for user controller

---
+++ backend/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
+  moduleNameMapper: {
+    "^src/(.*)$": "<rootDir>/src/$1"
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+};
+
+export default config;
